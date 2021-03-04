9fe45b720cf219a839e9a398d48aa8e31ed307f6
Benedikt <benediktsteinmueller@googlemail.com>
2021-03-04 18:37:14 +0100

    Added new activation functions from the notebook

--- neural_network_package/nn_activations.py
+++ neural_network_package/nn_activations.py
@@ -4,3 +4,13 @@ import numpy as np
 def sigmoid(z):
     """Returns sigmoid of given z"""
     return 1 / (1 + np.e ** -z)
+
+
+def tanh(z):
+    """ returns tan of given z"""
+    return np.tanh(z)
+
+
+def relu(z):
+    """Rectifying Linear Unit"""
+    return np.maximum(0, z)
