c9412418f1311d3b9132650d9ff15148dba4ec26
Benedikt <benediktsteinmueller@googlemail.com>
2021-04-07 14:17:51 +0200

    added concatenate frames ro shaping tools

--- neural_network_package/nn_shaping_tools.py
+++ neural_network_package/nn_shaping_tools.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 
 
 def add_bias_column(x):
@@ -6,6 +7,24 @@ def add_bias_column(x):
     Adds Bias of 1 as first Column of Array
     """
     return np.c_[np.ones(len(x)), x]
+
+
+def concatenate_frames(X, frame_amount):
+    """
+    Concatenates next frames to row of current frame
+
+    """
+    combined_frames = X.loc[0:]
+    for i in range(1, frame_amount):
+        additional_frame = X.loc[i:].reset_index(drop=True)
+        combined_frames = pd.concat(
+            [combined_frames, additional_frame], axis=1)
+
+    # Delete Last n Rows
+    combined_frames.drop(combined_frames.tail(
+        frame_amount).index, inplace=True)
+    # Convert to numpy array
+    return combined_frames.values
 
 
 def initialize_random_thetas(shape):
@@ -20,5 +39,3 @@ def initialize_random_thetas(shape):
         theta_list.append(np.random.rand((shape[i]) + 1, shape[i + 1])-0.5)
 
     return theta_list
-
-
