2a1e4d397c1e3a2e93bd2f2bcfb1c137a93a93bf
Sandra Birnstiel <sandra.birnstiel@stud-mail.uni-wuerzburg.de>
2021-04-17 13:34:28 +0200

    catch f1 score error if 0 predictions are correct

--- neural_network_package/nn_evaluation.py
+++ neural_network_package/nn_evaluation.py
@@ -18,6 +18,8 @@ def f1_score(h, y, true_negative_value=0):
     true_positives, false_positives, true_negatives, false_negatives = determine_true_false_positive_negative(h, y,
                                                                                                               true_negative_value)
 
+    if true_positives <= 0 or false_positives <= 0 or false_negatives <= 0:
+        return 0
     precision = true_positives / (true_positives + false_positives)
     recall = true_positives / (true_positives + false_negatives)
 
@@ -50,4 +52,3 @@ def determine_true_false_positive_negative(h, y, true_negative_value):
                 false_negatives += 1
 
     return true_positives, false_positives, true_negatives, false_negatives
-
