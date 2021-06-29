bab2473cecf0c91bb4854b1b7c1fbc3827f07d41
Benedikt Steinmüller <42609302+benni-dev-ux@users.noreply.github.com>
2021-06-29 16:20:09 +0200

    Updated Version number and setup
    Updated Package to Version Number 1.0
    Updated Contact Information

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
 setup(name='neural_network_package',
-      version='0.16',
+      version='1.0',
       description='Neural Net Package of Group 07',
-      url='https://gitlab2.informatik.uni-wuerzburg.de/hci/teaching/lectures/machine-learning/student-material/ws20/Team-07/neural_network_package',
+      url='https://github.com/benni-dev-ux/neural_network_package',
       author='Benedikt Steinmüller; Sandra Birnstiel; Lora Shishkova',
       author_email='---@example.com',
       license='--',
